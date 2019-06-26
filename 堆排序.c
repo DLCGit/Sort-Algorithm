fb415164c4218e9031d8076caec290002c8b5d71
Lfeng <40419358+LfengC@users.noreply.github.com>
2019-06-27 01:51:40 +0800

    Update 堆排序.c

--- 堆排序.c
+++ 堆排序.c
@@ -1,135 +1,70 @@
-
 /* 堆排序为不稳定排序: 平均、最坏、最好时间复杂度 O( n ),
  * 空间复杂度 O( 1 )
 */
 
-/* -----------------------------------------------------------  */
-/* 相关接口  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* -----------------------------------------------------------  */
+#define MAXN 10
+#define RANDOMMAXN 100
+#define swap(j, i) { int tmp = 0; tmp = i; i = j; j = tmp; }
 
+void init(int *base) {
+	srand(time(NULL));
+	int i;
+    for (printf("排序前: "), i = 1; i <= MAXN; base[ i ] = rand( ) % RANDOMMAXN, printf("%d ", base[ i ]), ++i); 
+    printf("\n");
+}
 
-/* -----------------------------------------------------------  */
-/* 相关特殊宏  */
-#define MAXSIZE 10
-#define RANDOMMAXSIZE 100
+void print(int *base, int size) {
+    int i;
+    for (i = size; i > 0; printf("%d ", base[ i-- ]));  
+    printf("\n");
+}
 
-#define swap( j, i ) { \
-int tmp = 0; tmp = i; \
-i = j; j = tmp; }
-/* -----------------------------------------------------------  */
+/* 创建堆 */
+void create_heap(int *base, int *heap) {
+	int i;
+    for (i = 1; i <= MAXN; heap[ i ] = base[ i ], ++i) {
+		int s = i, p = i / 2;
+		while (s >= 2 && heap[ s ] < heap[ p ]) {
+			swap(heap[ p ], heap[ s ]); 
+			s = p, p = s / 2;
+		}
+    }
+    for (i = 0; i <= MAXN; base[ i ] = heap[ i ], ++i); 
+}
 
+/* 建立堆  */
+void build_heap(int *base) {
+    create_heap(base, base);
+    int i;
+    for (printf("建立树堆: "), i = 1;  i <= MAXN; printf("%d ", base[ i++ ])); 
+    printf("\n");
+}
 
-/* -----------------------------------------------------------  */
-/* 全局  */
-/*extern*/ int heap[ MAXSIZE + 1 ] = { -1 }; /* 堆数组  */
-/* -----------------------------------------------------------  */
+/* 堆排 */
+void ordering(int *base) {
+    int m = MAXN;
+    while (m >= 1) {
+        swap( base[ 1 ], base[ m ] );
+		m--;
+		int p = 1, s = 2 * p;
+		while (m >= s) {
+		    if (m > s && base[ s ] > base[ s + 1 ]) s++;
+		    if (base[ s ] >= base[ p ]) break;
+		    swap(base[ p ], base[ s ]);
+		    p = s, s = 2 * p;
+		}
+		print(base, MAXN);
+    }
+    printf("\n");
+}
 
-
-/* -----------------------------------------------------------  */
-/* 函数原型  */
-void CreateHeap( int *data/*, int *heap*/ );         /* 创建堆  */
-void EstablishHeapTree( void );                      /* 建立堆  */
-void BeforeOrdering( void );                         /* 排序前  */
-void HeapSort( int *data );                          /* 堆排序  */
-void IsSorted( int *data );                          /* 排序后  */
-/* -----------------------------------------------------------  */
-
-
-/* -----------------------------------------------------------  */
-/* 主测试  */
-int main( int argc ,char **argv )
-{
-    BeforeOrdering();
-    EstablishHeapTree();
-    HeapSort( heap );
-	
-    system("pause");
+int main( ) {
+	int heap[ MAXN + 1 ] = { -1 }; 
+	init(heap);
+    build_heap(heap);
+    ordering(heap);
     return 0;
 }
-/* -----------------------------------------------------------  */
-
-
-/* -----------------------------------------------------------  */
-/* 创建堆  */
-void CreateHeap( int *data/*, int *heap*/ ) {
-    for ( int i = 1; MAXSIZE >= i; ++i )  {
-	heap[ i ] = data[ i ];
-	int s = i, p = i / 2;
-	while ( ( 2 <= s ) and ( heap[ s ] < heap[ p ] ) ) {
-	    swap( heap[ p ], heap[ s ] );
-	    s = p;
-	    p = s / 2;
-	}
-    }
-    
-    for ( int i = 0; MAXSIZE >= i; ++i )
-	data[ i ] = heap[ i ];
-}
-/* -----------------------------------------------------------  */
-
-
-/* -----------------------------------------------------------  */
-/* 建立堆  */
-void EstablishHeapTree( void ) {
-    CreateHeap( heap );
-    printf( "\n建立树堆: " );
-    for (int i = 1; MAXSIZE >= i; ++i )
-	printf( "%d \t", heap[ i ] );
-    putchar( '\n' );
-}
-/* -----------------------------------------------------------  */
-
-
-/* -----------------------------------------------------------  */
-/* 排序前  */
-void BeforeOrdering( void ) {
-    srand( ( unsigned int )time( NULL ) );
-    printf( "排序前:   " );
-    for ( int i = 1; MAXSIZE >= i; ++i ) {
-	heap[ i ] = rand() % RANDOMMAXSIZE;
-        printf( "%d \t", heap[ i ] );
-    }
-}
-/* -----------------------------------------------------------  */
-
-
-/* -----------------------------------------------------------  */
-/* 堆排序  */
-void HeapSort( int *data ) {
-    int m = MAXSIZE;
-    while ( 1 <= m ) 
-    {
-        swap( data[ 1 ], data[ m ] );
-	m--;
-
-	int p = 1, s = 2 * p;
-	while ( m >= s ) 
-	{
-	    if ( ( m > s ) and ( data[ s ] > data[ 1 + s ] ) )
-	        s++;
-	    if ( data[ s ] >= data[ p ] )
-		break;
-
-	    swap( data[ p ], data[ s ] );
-	    p = s;
-	    s = 2 * p;
-	}
-	IsSorted( data );
-    }
-    putchar( '\n' );
-}
-/* -----------------------------------------------------------  */
-
-
-/* -----------------------------------------------------------  */
-/* 排序后  */
-void IsSorted( int *data ) {
-    printf( "\n排序后: " );
-    for ( int i = MAXSIZE; 0 < i; --i )
-        printf( "%d   ", data[ i ] );
-}
-/* -----------------------------------------------------------  */
-
